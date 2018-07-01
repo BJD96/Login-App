bb8062aa6a21e41ebd55657d9e8eba73b5fe4647
Tomek <Thomas96b@gmail.com>
2018-07-01 11:33:31 +0200

    final

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -10,9 +10,9 @@ export class AuthenticationService {
     login(username: string, password: string) {
         return this.http.post<any>('/api/authenticate', { username: username, password: password })
             .map(user => {
-                // login successful if there's a jwt token in the response
+
                 if (user && user.token) {
-                    // store user details and jwt token in local storage to keep user logged in between page refreshes
+
                     localStorage.setItem('currentUser', JSON.stringify(user));
                 }
  
